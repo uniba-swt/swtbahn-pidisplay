849d0c687bb1165c56af5d00938682a3fe1d4fba
Eugene Yip <eyip002@aucklanduni.ac.nz>
2022-04-14 15:36:44 +0200

    Updated display for SWTbahn Full with shutdown and colour backlight

--- swtbahn-full/displayIpAddress.py
+++ swtbahn-full/displayIpAddress.py
@@ -4,8 +4,11 @@ import time
 import traceback
 import socket
 import datetime
+import os
 
 import dothat.lcd as lcd
+import dothat.touch as touch
+import dothat.backlight as backlight
 
 def tryGetIPAddress():
 	try:
@@ -13,31 +16,51 @@ def tryGetIPAddress():
 	except:
 		return 0
 
+@touch.on(touch.BUTTON)
+def handle_left(channel, event):
+	lcd.clear()
+	backlight.rgb(255, 0, 0)
+	lcd.set_cursor_position(1, 1)
+	lcd.write("Shutting Down!")
+	
+	global running
+	running = False
+	os.system("shutdown now")
+
+running = True
+
 try:
 	lcd.clear()
 	lcd.set_contrast(50)
+
 	
-	# Wait for IP address
-	if not tryGetIPAddress():
-		# Draw warning
-		lcd.set_cursor_position(0,0)
-		lcd.write("No IP address")
-	while not tryGetIPAddress():
+	while (running):
+		# Wait for IP-Address
+		if not tryGetIPAddress():
+			# Draw warning
+			backlight.rgb(0, 255, 255)
+	
+			lcd.set_cursor_position(0, 0)
+			lcd.write("No IP address")
+		else:
+			backlight.rgb(255, 255, 220)
+			backlight.single_rgb(0, 255, 0, 0)
+			
+			# Collect information
+			host_name = socket.gethostname() 
+			host_ip = tryGetIPAddress()
+			date_time = str(datetime.datetime.now())[:16] 
+			
+			# Display IP information
+			lcd.clear()
+			lcd.set_cursor_position(0,0)
+			lcd.write(host_name)
+			lcd.set_cursor_position(0,1)
+			lcd.write(host_ip)
+			lcd.set_cursor_position(0,2)
+			lcd.write(date_time)
+			
 		time.sleep(10)
-
-	# Collect information
-	host_name = socket.gethostname() 
-	host_ip = tryGetIPAddress()
-	date_time = str(datetime.datetime.now())[:16] 
-
-	# Display IP information
-	lcd.clear()
-	lcd.set_cursor_position(0,0)
-	lcd.write(host_name)
-	lcd.set_cursor_position(0,1)
-	lcd.write(host_ip)
-	lcd.set_cursor_position(0,2)
-	lcd.write(date_time)
         
 except:
 	print('traceback.format_exc():\n%s',traceback.format_exc())

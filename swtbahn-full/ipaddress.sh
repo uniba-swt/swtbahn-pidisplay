925994e9363e299817610e66f9f7d912feb3c763
Jochen Mehlich <contact@jochenmehlich.de>
2023-01-27 11:20:49 +0100

    fixed sh bug

--- swtbahn-full/ipaddress.sh
+++ swtbahn-full/ipaddress.sh
@@ -2,8 +2,9 @@ IP_ADDRESS=$(hostname -I | awk '{print $1}')
 while [ "$IP_ADDRESS" = "" ]
 do 
  IP_ADDRESS=$(hostname -I | awk '{print $1}')
- IP_ADDRESS2=$( IP_ADDRESS=$(hostname -I | awk '{print $2}')
+ IP_ADDRESS2=$(hostname -I | awk '{print $2}')
  sleep 1
 done
 echo $IP_ADDRESS
+echo $IP_ADDRESS2
 zenity --info --text="<span font=\"32\"><b>$IP_ADDRESS</b></br><b>$IP_ADDRESS2</b></span>" --title="IP Address"

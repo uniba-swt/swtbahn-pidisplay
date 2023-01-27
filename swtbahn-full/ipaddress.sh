6b8e8cd7576b7613e51620d03550f8ea300fdf18
Jochen Mehlich <contact@jochenmehlich.de>
2023-01-27 10:16:39 +0100

    added second ip to gui ip tool

--- swtbahn-full/ipaddress.sh
+++ swtbahn-full/ipaddress.sh
@@ -2,7 +2,8 @@ IP_ADDRESS=$(hostname -I | awk '{print $1}')
 while [ "$IP_ADDRESS" = "" ]
 do 
  IP_ADDRESS=$(hostname -I | awk '{print $1}')
+ IP_ADDRESS2=$( IP_ADDRESS=$(hostname -I | awk '{print $2}')
  sleep 1
 done
 echo $IP_ADDRESS
-zenity --info --text="<span font=\"32\"><b>$IP_ADDRESS</b></span>" --title="IP Address"
+zenity --info --text="<span font=\"32\"><b>$IP_ADDRESS</b></br><b>$IP_ADDRESS2</b></span>" --title="IP Address"

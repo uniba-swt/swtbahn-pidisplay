e16f7241f70835a6a54525118bbf49081cafdd39
Jochen Mehlich <contact@jochenmehlich.de>
2023-01-27 10:55:16 +0100

    added function fixes with formatting

--- swtbahn-full/displayIpAddress.py
+++ swtbahn-full/displayIpAddress.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import time
 import traceback
 import socket
@@ -31,9 +29,10 @@ def handle_quit(channel, event):
 
 max_len = 0
 counter = 0
-@touch.on(touch.NEXT)
-def changeInterface():
+@touch.on(touch.UP)
+def changeInterface(channel, event):
 	global max_len, counter
+	print("change interface from {} to {}".format(counter, counter+1))
 	if counter is max_len:
 		counter = 0
 	else:
@@ -73,32 +72,36 @@ try:
 			
 			lcd.set_cursor_position(0, 0)
 			lcd.write("No IP address")
-		else:
-			backlight.rgb(140, 170, 170)
-			
-			# Collect information
-			adapters = ifaddr.get_adapters()
-			ipCollection = []
-			for adapter in adapters:
-				for ip in adapter.ips:
-					ipCollection.append([ip, adapter.nice_name])
-			max_len = len(ipCollection)
-			host_name = None
-			if len(ipCollection) > 0:
-				host_name = ipCollection[counter][1]
-				host_ip = ipCollection[counter][0]
-			date_time = str(datetime.datetime.now())[:16] 
-			
+#		else:
+		backlight.rgb(140, 170, 170)
+		print("Get adapters")
+		# Collect information
+		adapters = ifaddr.get_adapters()
+		ipCollection = []
+		for adapter in adapters:
+			ipCollection.append([adapter.ips[0].ip, adapter.nice_name])
+
+		max_len = len(ipCollection)
+		host_name = None
+		if counter >= len(ipCollection):
+			counter = 0
+		if len(ipCollection) > 0:
+			host_name = ipCollection[counter][1]
+			host_ip = str(ipCollection[counter][0])
+		date_time = str(datetime.datetime.now())[:16] 
+		print(host_ip)
+		print(counter)
 			# Display IP information
-			lcd.set_cursor_position(0,0)
-			lcd.write(host_name)
-			lcd.set_cursor_position(0,1)
-			lcd.write(host_ip)
-			lcd.set_cursor_position(0,2)
-			lcd.write(date_time)
+		lcd.set_cursor_position(0,0)
+		lcd.write(host_name)
+		lcd.set_cursor_position(0,1)
+		lcd.write(host_ip)
+		lcd.set_cursor_position(0,2)
+		lcd.write(date_time)
 			
 		time.sleep(10)
         
 except:
 	print('traceback.format_exc():\n%s',traceback.format_exc())
 	exit()
+

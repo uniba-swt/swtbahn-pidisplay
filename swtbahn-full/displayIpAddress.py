5fa2b6f2bba7fe08e3f83129f03e8676efe8fc5d
Eugene Yip <eyip002@aucklanduni.ac.nz>
2023-01-27 16:24:32 +0100

    Code clean up

--- swtbahn-full/displayIpAddress.py
+++ swtbahn-full/displayIpAddress.py
@@ -4,57 +4,54 @@ import datetime
 import os
 import ifaddr
 
-
 import dothat.lcd as lcd
 import dothat.touch as touch
 import dothat.backlight as backlight
 
+
 networkInterfaceIndex = 0
 
+# Helper functions
 def updateDisplay():
-	global networkInterfaceIndex
 	lcd.clear()
 
-
-	# Collect information
+	# Collect IP information
 	adapters = ifaddr.get_adapters()
 	ipCollection = []
 
 	for adapter in adapters:
 		ipCollection.append([adapter.ips[0].ip, adapter.nice_name])
-	
-	# Definition of Vars #+defho
-		
-	host_ip = None
-	host_name = None
-	date_time = str(datetime.datetime.now())[:16] 
-	# Validating the networkInterfaceIndex in Range of IPs
+
+	# Restrict networkInterfaceIndex to the range of available IPs
+	global networkInterfaceIndex
 	if networkInterfaceIndex >= len(ipCollection):
 		networkInterfaceIndex = 0
 
+	host_ip = None
+	host_name = None
 	if len(ipCollection) > 0:
-		host_name = ipCollection[networkInterfaceIndex][1]
 		host_ip = str(ipCollection[networkInterfaceIndex][0])
+		host_name = ipCollection[networkInterfaceIndex][1]
 
 	if host_ip is None:
-		# When no ip is found
+		# No IP found
 		backlight.rgb(170, 170, 0)
 		
 		lcd.set_cursor_position(0, 0)
 		lcd.write("No IP address")
-
 	else:
+		# IP found
 		backlight.rgb(140, 170, 170)
-
-		# Write Ip Information
+		
+		# Write IP Information
 		lcd.set_cursor_position(0,0)
 		lcd.write(host_name)
 		lcd.set_cursor_position(0,1)
 		lcd.write(host_ip)
 		lcd.set_cursor_position(0,2)
+		date_time = str(datetime.datetime.now())[:16] 
 		lcd.write(date_time)
 
-# Helper functions
 def blinkLed():
 	backlight.graph_set_led_duty(0, 1)
 	
@@ -64,6 +61,7 @@ def blinkLed():
 		backlight.graph_set_led_state(0, 0)
 		time.sleep(0.5)
 
+# Display buttons
 @touch.on(touch.CANCEL)
 def handle_quit(channel, event):
 	backlight.off()
@@ -93,7 +91,6 @@ try:
 	backlight.graph_off()
 	lcd.set_contrast(50)
 
-	
 	while (running):
 		updateDisplay()
 		time.sleep(10)

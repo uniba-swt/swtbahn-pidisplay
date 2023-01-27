77c0ffcfcc8849af70ce9b79ad2a7c8bcbcfe0a6
Jochen Mehlich <contact@jochenmehlich.de>
2023-01-27 11:32:57 +0100

    customized the ui ip display

--- swtbahn-full/ipaddress.sh
+++ swtbahn-full/ipaddress.sh
@@ -1,10 +1,11 @@
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
-while [ "$IP_ADDRESS" = "" ]
+IP_ADDRESS1=$(hostname -I | awk '{print $1}')
+IP_ADDRESS2=$(hostname -I | awk '{print $2}')
+while [ "$IP_ADDRESS1" = "" ]
 do 
- IP_ADDRESS=$(hostname -I | awk '{print $1}')
+ IP_ADDRESS1=$(hostname -I | awk '{print $1}')
  IP_ADDRESS2=$(hostname -I | awk '{print $2}')
  sleep 1
 done
-echo $IP_ADDRESS
+echo $IP_ADDRESS1
 echo $IP_ADDRESS2
-zenity --info --text="<span font=\"32\"><b>$IP_ADDRESS</b></br><b>$IP_ADDRESS2</b></span>" --title="IP Address"
+zenity --info --text="<span font=\"32\"><b>$IP_ADDRESS1</b> <b>$IP_ADDRESS2</b></span>" --title="IP Addresses"
